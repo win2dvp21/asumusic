1d87eed7ead49d7afffe5fb466fca0febe7d481c
SeungYoun Lee <win2dvp21@gmail.com>
2023-11-08 19:23:21 +0900

    Added page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Main from './pages/main';
+import Create from './pages/create';
+import Detail from './pages/detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/main/*" element={<Main />}></Route>
+        <Route path="/create/*" element={<Create />}></Route>
+        <Route path="/detail/*" element={<Detail />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
